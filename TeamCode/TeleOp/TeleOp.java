55a4dd7a0fab1c35830ab007bb9426d7d62eacbc
Squiddy <49382935+ASquidboi@users.noreply.github.com>
2024-11-09 13:43:20 -0500

    Update TeleOp.java

--- TeamCode/TeleOp/TeleOp.java
+++ TeamCode/TeleOp/TeleOp.java
@@ -34,9 +34,9 @@ public class TeleOp2 extends LinearOpMode {
 
         while (opModeIsActive()) {
             //left_trigger, right_
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is (no longer) reversed
-            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = -gamepad2.right_stick_x;
+            double y = gamepad1.right_stick_y; // Remember, Y stick value is (no longer) reversed
+            double x = -gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = -gamepad1.left_stick_x;
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
