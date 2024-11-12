1cf6b102f3fc1cf8d6bd8c3e141d8a338afda288
Squiddy <49382935+ASquidboi@users.noreply.github.com>
2024-11-11 22:37:18 -0500

    this code is gonna be permanently engraved in my brain

--- TeamCode/TeleOp/LinearArmController.java
+++ TeamCode/TeleOp/LinearArmController.java
@@ -3,56 +3,56 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-public class LinearArmController {
+public class LinearSlideController {
     public static /*final*/ int TARGET_POSITION_TICKS = 0; // For 4 feet with a specific ticks-per-foot calculation
 
-    private DcMotor armMotor;
+    private DcMotor slideMotor;
     
     int MAX_EXTEND_HEIGHT = 3850;
 
-    public LinearArmController(DcMotor motor) {
-        this.armMotor = motor;
+    public LinearSlideController(DcMotor motor) {
+        this.slideMotor = motor;
 
         // Reset encoder to start at zero
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         // Set motor direction if needed
-        armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        slideMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Set motor to RUN_TO_POSITION mode for position control
-        armMotor.setTargetPosition(TARGET_POSITION_TICKS);
+        slideMotor.setTargetPosition(TARGET_POSITION_TICKS);
         
 
         // Set power for the motor to start moving towards the target
-        //armMotor.setPower(0);
+        //slideMotor.setPower(0);
     }
 
-    public void extendArm() {
+    public void extendSlide() {
         // Start moving the arm to the target position
-        armMotor.setTargetPosition(TARGET_POSITION_TICKS);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        if (armMotor.getCurrentPosition() < MAX_EXTEND_HEIGHT) {
-            armMotor.setPower(1); // Full power to reach target
+        slideMotor.setTargetPosition(TARGET_POSITION_TICKS);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if (slideMotor.getCurrentPosition() < MAX_EXTEND_HEIGHT) {
+            slideMotor.setPower(1); // Full power to reach target
         }
-        if (armMotor.getCurrentPosition() <= 0 && TARGET_POSITION_TICKS == 0) { //Check if we're at/less than 0 and we want to be at 0. If so, turn off motor to reduce strain.
-            armMotor.setPower(0);
+        if (slideMotor.getCurrentPosition() <= 0 && TARGET_POSITION_TICKS == 0) { //Check if we're at/less than 0 and we want to be at 0. If so, turn off motor to reduce strain.
+            slideMotor.setPower(0);
         }
     }
     
 
     public void stopIfReached() {
         // Check if the arm has reached its target position
-        if (!armMotor.isBusy()) {
+        if (!slideMotor.isBusy()) {
             // If the motor is no longer busy, stop the motor
-            armMotor.setPower(0);
-            //armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset encoder if needed
+            slideMotor.setPower(0);
+            //slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset encoder if needed
         }
-        if (armMotor.getCurrentPosition() > MAX_EXTEND_HEIGHT) {
-            armMotor.setPower(0);
+        if (slideMotor.getCurrentPosition() > MAX_EXTEND_HEIGHT) {
+            slideMotor.setPower(0);
         }
-        if (armMotor.getCurrentPosition() < 0) {
-            armMotor.setPower(0);
+        if (slideMotor.getCurrentPosition() < 0) {
+            slideMotor.setPower(0);
         }
     }
 }
